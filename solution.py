fd6387b7ec8e7b2273a7b5bc8796ddb783b4e534
asvcode <avirdee@aol.com>
2017-09-06 17:02:04 -0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -147,29 +147,6 @@ def only_choice(values):
             if len(dplaces) == 1:
                 values[dplaces[0]] = digit
     return values
-"""
-def search(values):
-    Using depth-first search and propagation, create a search tree and solve the sudoku."
-    First, reduce the puzzle using the previous function
-    
-    Choose one of the unfilled squares with the fewest possibilities
-    
-    Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
-    
-    values = reduce_puzzle(values)
-    if values is False:
-        return False ## Failed earlier
-    if all(len(values[s]) == 1 for s in boxes): 
-        return values ## Solved!
-    # Choose one of the unfilled squares with the fewest possibilities
-    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
-    # Now use recurrence to solve each one of the resulting sudokus, and 
-    for value in values[s]:
-        new_sudoku = values.copy()
-        new_sudoku[s] = value
-        attempt = search(new_sudoku)
-        if attempt:
-            return attempt"""
 
 def search(values):
     # First, reduce the puzzle using the previous function
